f47b60c54e091ecc7f1a64d269c6e72d093e8ee6
simonezanfagna <zanfagnasimone@live.it>
2019-11-18 18:59:24 +0100

    fine metodo 2

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 var cognome = prompt("Inserisci il tuo cognome");
 var lista_cognomi = ['Rossi', 'Ferrari', 'Russo', 'Bianchi'];
-lista_cognomi.push(cognome);
-var lista_aggionata = lista_cognomi.sort();
-console.log(lista_aggionata);
-//console.log(cognome + ' posizione = ' + (parseInt(lista_cognomi.indexOf(cognome)) + 1));
-console.log(cognome + ' indice = ' + lista_aggionata.indexOf(cognome));
+
+
+// Metodo 1
+
+// lista_cognomi.push(cognome);
+// var lista_aggionata = lista_cognomi.sort();
+// console.log(lista_aggionata);
+// //console.log(cognome + ' posizione = ' + (parseInt(lista_cognomi.indexOf(cognome)) + 1));
+// console.log(cognome + ' indice = ' + lista_aggionata.indexOf(cognome));
+
+
+
+// Metodo 2
+var c = 0;
+while (c <= 0){
+  lista_cognomi.push(cognome);
+  var lista_aggionata = lista_cognomi.sort();
+  console.log(lista_aggionata);
+
+  for (i = 0; i < lista_aggionata.length; i++) {
+
+    if (lista_aggionata[i] == cognome) {
+      console.log(cognome + ' indice = ' + i);
+    }
+  }
+
+  c = 1;
+}
